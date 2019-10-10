e367714dfb49b539073f3daac3efb003a493a914
Nate V <navogel@gmail.com>
2019-10-10 01:36:38 -0500

    added basic styling to nav, added basic drawer to test functionality

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,40 +1,52 @@
-import React, { Component } from "react"
-import { Link, withRouter } from "react-router-dom"
-import "bootstrap/dist/css/bootstrap.min.css"
-import FriendsList from "../friends/FriendsList"
-
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FriendsList from '../friends/FriendsList';
+import { Button } from 'antd';
 
 class NavBar extends Component {
-    handleLogout = () => {
-        console.log("handleLogout is being called" , this.props)
-        this.props.clearUser();
-        this.props.history.push('/');
-      }
-    render() {
-        return (
-            <div  className="navWrapper">
-                <div className="logoPlaceholder">Logo Placeholder</div>
-            <nav>
-                <ul className="nav nav-pills nav-fill">
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/news">News</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/events">Events</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/messages">Messages</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/tasks">Tasks</Link>
-                    </li>
-                    <button onClick={this.handleLogout}>Logout</button>
-                </ul>
-            </nav>
-            <FriendsList />
-            </div>
-        )
-    }
+	handleLogout = () => {
+		console.log('handleLogout is being called', this.props);
+		this.props.clearUser();
+		this.props.history.push('/');
+	};
+	render() {
+		return (
+			<div className='navWrapper'>
+				<div className='logoPlaceholder'>Logo Placeholder</div>
+				<nav>
+					<ul className='nav nav-pills nav-fill'>
+						<li className='nav-item'>
+							<Link className='nav-link' to='/news'>
+								<Button className='navBtn'>News</Button>
+							</Link>
+						</li>
+						<li className='nav-item'>
+							<Link className='nav-link' to='/events'>
+								<Button className='navBtn'>Events</Button>
+							</Link>
+						</li>
+						<li className='nav-item'>
+							<Link className='nav-link' to='/messages'>
+								<Button className='navBtn'>Messages</Button>
+							</Link>
+						</li>
+						<li className='nav-item'>
+							<Link className='nav-link' to='/tasks'>
+								<Button className='navBtn'>Tasks</Button>
+							</Link>
+						</li>
+						<li className='nav-item'>
+							<Button size='small' onClick={this.handleLogout}>
+								Logout
+							</Button>
+						</li>
+					</ul>
+				</nav>
+				<FriendsList />
+			</div>
+		);
+	}
 }
 
 export default withRouter(NavBar);
