7ec090ce27e3f242e5af207d7aed8e92600724c2
Nate V <navogel@gmail.com>
2019-10-14 23:19:04 -0500

    animation to friend search via react Spring

--- src/components/friends/FriendSearchCard.js
+++ src/components/friends/FriendSearchCard.js
@@ -1,20 +1,33 @@
 // This file is in charge of displaying one friend that is being pulled
 //from FriendsSearch. It will also contain an Add button, and their name.
 import React, { Component } from 'react';
+import { Button } from 'antd';
+import { Spring } from 'react-spring/renderprops';
 
 class FriendSearchCard extends Component {
 	render() {
 		return (
-			<div className='friendSearchRow'>
-				<h5>{this.props.friend.userName}</h5>
-				<button
-					type='button'
-					className='btn'
-					onClick={() => this.props.addFriend(this.props.friend.id)}
-				>
-					Haunt
-				</button>
-			</div>
+			<Spring
+				from={{ opacity: 0 }}
+				to={{ opacity: 1 }}
+				//config={{ duration: 600, easing: easings.easeCubic }}
+			>
+				{props => (
+					<div className='friendSearchWrapper' style={props}>
+						<div className='friendSearchRow'>
+							<h5>{this.props.friend.userName}</h5>
+							<Button
+								size='small'
+								type='button'
+								className='btn'
+								onClick={() => this.props.addFriend(this.props.friend.id)}
+							>
+								Haunt
+							</Button>
+						</div>
+					</div>
+				)}
+			</Spring>
 		);
 	}
 }
